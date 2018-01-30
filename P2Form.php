30593e26b636bb0ecb11bfd6b9c064afd3e53745
Jordan Blackmon <disasterisk@users.noreply.github.com>
2018-01-30 13:30:15 -0800

    Add files via upload

--- P2Form.php
+++ P2Form.php
@@ -1,4 +1,118 @@
 <?php
+//saladTruck.php
+define('THIS_PAGE', basename($_SERVER['PHP_SELF'])); //figure out the name of the current page
+$greens[] = new Item(1,"Iceberg Lettuce",0);
+$greens[] = new Item(2,"Romaine Lettuce",0);
+$greens[] = new Item(3,"Spinach",0);
+$greens[] = new Item(4,"Arugula",0);
+$veggies[] = new Item(5,"Tomato",0);
+$veggies[] = new Item(6,"Cucumber",0);
+$veggies[] = new Item(7,"Bell Pepper",0);
+$veggies[] = new Item(8,"Mushroom",0);
+$meat[] = new Item(9,"Chicken",1.5);
+$meat[] = new Item(10,"Salmon",1.5);
+$meat[] = new Item(11,"Steak",2);
+$meat[] = new Item(15,"Shrimp",1.5);
+$extras[] = new Item(12,"Cheddar Cheese",.25);
+$extras[] = new Item(13,"Feta Cheese",.5);
+$extras[] = new Item(14,"Croutons",0);
+class Item
+{
+    public $ID = 0;
+    public $Name = '';
+    public $Price = 0;
+
+
+    public function __construct($ID,$Name,$Price)
+    {
+        $this->ID = $ID;
+        $this->Name = $Name;
+        $this->Price = $Price;
+
+    }#end Item constructor
+
+}#end Item class
+if (!isset($_POST['submit'])) //if nothing is ordered, print the menu
+        {
+            echo '
+            <form action = "' . $_SERVER['PHP_SELF'] . '"  method = "POST"> ';
+
+            foreach($greens as $tempItem)
+            {
+                createMenu($tempItem);
+            }
+
+            echo'<br>';
+
+            foreach($veggies as $tempItem)
+            {
+                createMenu($tempItem);
+            }
+
+            echo'<br>';
+
+            foreach($meat as $tempItem)
+            {
+               createMenu($tempItem);
+            }
+
+            echo'<br>';
+
+            foreach($extras as $tempItem)
+            {
+                createMenu($tempItem);
+            }
+
+            echo'<br>';
+
+
+		echo '
+	    <br>
+      <select name="amount">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+      </select>&nbsp;
+      <input type = "submit" name = "submit" value ="Submit Order">
+    	</form>   ';
+        }//if statement to generate menu + order form
+		else
+        {
+            if(isset($_POST['submit'])){
+                if(!empty($_POST['menuItems'])) {
+                    $sum = 5.99;
+                    foreach($_POST['menuItems'] as $value){
+                    //pull values of the checkboxes
+                    //echo "value : ".$value.'<br/>';
+                      $sum += $value;//Add up the total
+                    }//end of foreach
+                }//end of if
+            }//end of if
+            $sum = $sum * intval($_POST['amount']);
+            $subtotal = $sum;
+            $tax = $sum*.096;
+            $total = $subtotal + $tax;
+            echo 'Subtotal: $' . number_format($sum, 2) . '';
+            echo '<br>';
+            echo 'Tax: $' . number_format($tax, 2) . '';
+            echo '<br>';
+            echo 'Total: $' . number_format($total, 2) . '';
+
+            echo'<br>';
+            echo '<a href='. $_SERVER['PHP_SELF'] .'> Place Another Order! </a>';
+        }//items ordered, display totals
+
+function createMenu($tempItem){
+    echo'
+    <input type="checkbox" name="menuItems[]" value="' . $tempItem->Price . '">' . $tempItem->Name . '
+    ';
+}
+/*
 echo '<form action="P2Form.php" method="post">
   <h1>Salads</h1>
   <h2>Salad Greens</h2>
@@ -78,3 +192,4 @@ echo '<form action="P2Form.php" method="post">
   <input type="submit" name="submitSalad">
 </form>
 ';
+*/
