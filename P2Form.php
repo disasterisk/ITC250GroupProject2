b3007073a1c6fd7518a30df51ab5373b254d0f08
Jordan Blackmon <disasterisk@users.noreply.github.com>
2018-01-30 18:34:57 -0800

    Update P2Form.php

--- P2Form.php
+++ P2Form.php
@@ -1,21 +1,24 @@
 <?php
 //saladTruck.php
 define('THIS_PAGE', basename($_SERVER['PHP_SELF'])); //figure out the name of the current page
-$greens[] = new Item(1,"Iceberg Lettuce",0);
-$greens[] = new Item(2,"Romaine Lettuce",0);
-$greens[] = new Item(3,"Spinach",0);
-$greens[] = new Item(4,"Arugula",0);
-$veggies[] = new Item(5,"Tomato",0);
-$veggies[] = new Item(6,"Cucumber",0);
-$veggies[] = new Item(7,"Bell Pepper",0);
-$veggies[] = new Item(8,"Mushroom",0);
-$meat[] = new Item(9,"Chicken",1.5);
-$meat[] = new Item(10,"Salmon",1.5);
-$meat[] = new Item(15,"Shrimp",1.5);
-$meat[] = new Item(11,"Steak",2);
-$extras[] = new Item(14,"Croutons",0);
-$extras[] = new Item(12,"Cheddar Cheese",.25);
-$extras[] = new Item(13,"Feta Cheese",.5);
+$greens[] = new Ingredient(1,"Iceberg Lettuce",0);
+$greens[] = new Ingredient(2,"Romaine Lettuce",0);
+$greens[] = new Ingredient(3,"Spinach",0);
+$greens[] = new Ingredient(4,"Arugula",0);
+$veggies[] = new Ingredient(5,"Tomato",0);
+$veggies[] = new Ingredient(6,"Cucumber",0);
+$veggies[] = new Ingredient(7,"Bell Pepper",0);
+$veggies[] = new Ingredient(8,"Mushroom",0);
+$meat[] = new Ingredient(9,"Chicken",1.5);
+$meat[] = new Ingredient(10,"Salmon",1.5);
+$meat[] = new Ingredient(15,"Shrimp",1.5);
+$meat[] = new Ingredient(11,"Steak",2);
+$extras[] = new Ingredient(14,"Croutons",0);
+$extras[] = new Ingredient(12,"Cheddar Cheese",.25);
+$extras[] = new Ingredient(13,"Feta Cheese",.5);
+$order = array();
+$subtotal = 0;
+//create list of ingredients for reference
 $db = array();
 foreach ($greens as $i) {
   $db[] = $i;
@@ -26,7 +29,8 @@ foreach ($greens as $i) {
 }foreach ($extras as $i) {
   $db[] = $i;
 }
-class Item
+
+class Ingredient
 {
     public $ID = 0;
     public $Name = '';
@@ -39,48 +43,73 @@ class Item
         $this->Name = $Name;
         $this->Price = $Price;
 
-    }#end Item constructor
+    }#end Ingredient constructor
 
-}#end Item class
-if (!isset($_POST['submit'])) //if nothing is ordered, print the menu
-        {
-            echo '
-            <form action = "' . $_SERVER['PHP_SELF'] . '"  method = "POST"> ';
-            echo '
-            <h1>Salads</h1>
-            <h2>$5.99</h2>
-            ';
-            foreach($greens as $tempItem)
-            {
-                createMenu($tempItem);
-            }
+}#end Ingredient class
 
-            echo'<br>';
+/**
+ *
+ */
+class Salad
+{
+  private $ingredients = array();
+  private $price = 0;
+  private $amount = 0;
+  function __construct($price,$amount)
+  {
+    $this->price = $price;
+    $this->amount = $amount;
+  }
+  public function getNgrdts() {
+    return $this->ingredients;
+  }
+  public function getPrice() {
+    return $this->price;
+  }
+  public function getAmount() {
+    return $this->amount;
+  }
+  public function addNgrdt($n) {
+    $this->ingredients[] = $n;
+  }
+}
 
-            foreach($veggies as $tempItem)
-            {
-                createMenu($tempItem);
-            }
+if (!isset($_POST['submit'])) //if nothing is ordered, show form
+{
+    echo '<form action = "' . $_SERVER['PHP_SELF'] . '"  method = "POST"> ';
+    echo '
+      <h1>Salads</h1>
+      <h2>$5.99</h2>
+    ';
+    foreach($greens as $tempNgrdt)
+    {
+        createMenu($tempNgrdt);
+    }
 
-            echo'<br>';
+    echo'<br>';
 
-            foreach($meat as $tempItem)
-            {
-               createMenu($tempItem);
-            }
+    foreach($veggies as $tempNgrdt)
+    {
+        createMenu($tempNgrdt);
+    }
 
-            echo'<br>';
+    echo'<br>';
 
-            foreach($extras as $tempItem)
-            {
-                createMenu($tempItem);
-            }
+    foreach($meat as $tempNgrdt)
+    {
+        createMenu($tempNgrdt);
+    }
 
-            echo'<br>';
+    echo'<br>';
 
+    foreach($extras as $tempNgrdt)
+    {
+        createMenu($tempNgrdt);
+    }
 
-		echo '
-	    <br>
+    echo'<br>';
+    echo '
+      <br>
       <select name="amount">
         <option value="1">1</option>
         <option value="2">2</option>
@@ -92,44 +121,66 @@ if (!isset($_POST['submit'])) //if nothing is ordered, print the menu
         <option value="8">8</option>
       </select>&nbsp;
       <input type = "submit" name = "submit" value ="Submit Order">
-    	</form>';
-        }//if statement to generate menu + order form
-		else
-        {
-          $salad = array();
-            if(isset($_POST['submit'])){
-                if(!empty($_POST['menuItems'])) {
-                    $sum = 5.99;
-                    foreach($_POST['menuItems'] as $value){
-                      foreach ($db as $item) {
-                        if ($value==$item->ID) {
-                          $sum += $item->Price;
-                          $salad[] = $item->Name;
-                        }
-                      }
-                    }//end of foreach
-                }//end of if
-            }//end of if
-            $sum = $sum * intval($_POST['amount']);
-            $subtotal = $sum;
-            $tax = $sum*.096;
-            $total = $subtotal + $tax;
-            echo implode(', ',$salad).'<br>';
-            echo 'Subtotal: $' . number_format($sum, 2) . '';
-            echo '<br>';
-            echo 'Tax: $' . number_format($tax, 2) . '';
-            echo '<br>';
-            echo 'Total: $' . number_format($total, 2) . '';
+      </form><br>
+    ';#end form
+}//end if
+else
+{
+    //variable to keep track of salad ingredients
+    $salad = array();
+    //executes if form has been submitted
+    if(isset($_POST['submit'])){
+        //executes if anything has been selected
+        if(!empty($_POST['menuNgrdts'])) {
+            //base salad price
+            $saladPrice = 5.99;
+            //look at selected ingredients
+            foreach($_POST['menuNgrdts'] as $value){
+                //search database for matches
+                foreach ($db as $ngrdt) {
+                    //find ingredient
+                    if ($value==$ngrdt->ID) {
+                        //add ingredient price to salad price
+                        $saladPrice += $ngrdt->Price;
+                        //add ingredient to list
+                        $salad[] = $ngrdt->Name;
+                        //signal end of search
+                        break;
+                    }//end ingredient lookup
+                }//end database search
+            }//end of ingredient lookup
+        }//end if for selected ingredients
+    }//end of check for $_POST
+    //store desired number of salads
+    $a = $_POST['amount'];
+    //multiply salad price by number ordered
+    $newSalad = new Salad($saladPrice,$a);
+    foreach ($salad as $s) {
+      $newSalad->addNgrdt($s);
+    }
+    $order[] = $newSalad;
+    $sum = $saladPrice * intval($a);
+    //add salad price to subtotal
+    $subtotal += $sum;
+    $tax = $sum*.096;
+    $total = $subtotal + $tax;
 
-            echo'<br>';
-            echo '<a href='. $_SERVER['PHP_SELF'] .'> Place Another Order! </a>';
-        }//items ordered, display totals
+    printSalad($newSalad);
+    echo 'Subtotal: $' . number_format($sum, 2) . '';
+    echo '<br>';
+    echo 'Tax: $' . number_format($tax, 2) . '';
+    echo '<br>';
+    echo 'Total: $' . number_format($total, 2) . '';
 
-function createMenu($tempItem){
-    $i = $tempItem->ID;
-    $p = $tempItem->Price;
+    echo'<br>';
+    echo '<a href='. $_SERVER['PHP_SELF'] .'> Add to order</a>';
+}//ingredients ordered, display totals
+
+function createMenu($tempNgrdt){
+    $i = $tempNgrdt->ID;
+    $p = $tempNgrdt->Price;
     echo'
-    <input type="checkbox" name="menuItems[]" value="'.$i.'">' . $tempItem->Name . '
+    <input type="checkbox" name="menuNgrdts[]" value="'.$i.'">'.$tempNgrdt->Name.'
     ';
     if ($p!= 0) {
       $d = 0;
@@ -139,84 +190,9 @@ function createMenu($tempItem){
     echo '$'.number_format($p,$d);
     }
 }
-/*
-echo '<form action="P2Form.php" method="post">
-  <h1>Salads</h1>
-  <h2>Salad Greens</h2>
-  <label>
-    <input type="checkbox" name="leaves" value="iceberg">&nbsp;
-    Iceberg Lettuce
-  </label>
-  <label>
-    <input type="checkbox" name="leaves" value="romaine">&nbsp;
-    Romaine Lettuce
-  </label>
-  <label>
-    <input type="checkbox" name="leaves" value="spinach">&nbsp;
-    Spinach
-  </label>
-  <label>
-    <input type="checkbox" name="leaves" value="arugula">&nbsp;
-    Arugula
-  </label>
-  <h2>Veggies</h2>
-    <label>
-      <input type="checkbox" name="veggie" value="tomato">&nbsp;
-      Tomato
-    </label>
-    <label>
-      <input type="checkbox" name="veggie" value="cucumber">&nbsp;
-      Cucumber
-    </label>
-    <label>
-      <input type="checkbox" name="veggie" value="bellpepper">&nbsp;
-      Bell Pepper
-    </label>
-    <label>
-      <input type="checkbox" name="veggie" value="mushroom">&nbsp;
-      Mushroom
-    </label>
-  <h2>Meat</h2>
-  <label>
-    <input type="radio" name="meat" value="chicken">&nbsp;
-    Chicken&nbsp;&nbsp;<small><em>$1</em></small>
-  </label>
-  <label>
-    <input type="radio" name="meat" value="salmon">&nbsp;
-    Salmon&nbsp;&nbsp;<small><em>$1</em></small>
-  </label>
-  <label>
-    <input type="radio" name="meat" value="shrimp">&nbsp;
-  </label>
-  <label>
-    <input type="radio" name="meat" value="steak">&nbsp;
-    Steak&nbsp;&nbsp;<small><em>$2</em></small>
-  </label>
-  <h2>Extras</h2>
-  <label>
-    <input type="checkbox" name="extra" value="cheddar">&nbsp;
-    Cheddar Cheese
-  </label>
-  <label>
-    <input type="checkbox" name="extra" value="feta">&nbsp;
-    Feta Cheese
-  </label>
-  <label>
-    <input type="checkbox" name="extra" value="croutons">&nbsp;
-    Croutons
-  </label>
-  <br><br>
-  <select name="amount">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-  </select>&nbsp;
-  <input type="submit" name="submitSalad">
-</form>
-';
-*/
+function printSalad($s){
+  $i = $s->getNgrdts();
+  echo
+    implode(', ',$i).'&nbsp;&times;&nbsp;'.$s->getAmount().
+    '&nbsp;&commat;&nbsp;$'.number_format($s->getPrice(),2).'<br>';
+}
